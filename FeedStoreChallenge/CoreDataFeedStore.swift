23b016aacdc306d3e450f3da7485901786d885fa
Tiago Amorim <tiago.amorim@wit-software.com>
2021-08-21 18:49:33 +0100

    'CoreDataFeedStore' delete has no side effects on deletion error

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -76,6 +76,7 @@ public final class CoreDataFeedStore: FeedStore {
 				try ManagedCache.find(in: context).map(context.delete).map(context.save)
 				completion(nil)
 			} catch {
+				context.rollback()
 				completion(error)
 			}
 		}
