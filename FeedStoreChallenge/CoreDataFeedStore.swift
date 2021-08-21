86509d34f0b2b2691da1e4e22604ab690cab4ae1
Tiago Amorim <tiago.amorim@wit-software.com>
2021-08-21 18:43:43 +0100

    'CoreDataFeedStore' delete delivers no error on empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -71,7 +71,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		fatalError("Must be implemented")
+		completion(nil)
 	}
 
 	private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
